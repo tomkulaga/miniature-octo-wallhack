b7cfb19a0cc96a5c9ba593538ff9aad1adfa6a9a
tomkulaga <tommy8927@gmail.com>
2013-11-16 00:39:17 +1100

    testing commit system

--- ClassLibrary1/Binding.cs
+++ ClassLibrary1/Binding.cs
@@ -15,7 +15,7 @@ namespace Binding
     public interface ISubject<out T>
         where T : EventArgs
     {
-        void Attach(IObserver<T> observer);
+        void Attach(IObserver<T> observer); 
         void Detach(IObserver<T> observer);
     }
 }
